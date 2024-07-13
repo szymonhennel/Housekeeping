c3a5529cda46862df5f8d73bfc8777ac97ce2a63
Szymon Hennel <szymon.hennel@gmail.com>
2024-07-13 17:22:45 +0200

    New function Move-Pictures for archiving pictures into YYYY-MM stamped folders

--- Housekeeping.psm1
+++ Housekeeping.psm1
@@ -1,5 +1,7 @@
 . "$PSScriptRoot\public\Move-CanonMP4.ps1"
 . "$PSScriptRoot\public\Sync-OfflineArchive.ps1"
+. "$PSScriptRoot\public\Move-Pictures.ps1"
 
 Export-ModuleMember -Function Move-CanonMP4
 Export-ModuleMember -Function Sync-OfflineArchive
+Export-ModuleMember -Function Move-Pictures
---
+++ public/Move-Pictures.ps1
@@ -0,0 +1,55 @@
+function Move-Pictures {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$SourceDirectory,
+        [Parameter(Mandatory=$true)]
+        [string]$DestinationDirectory
+    )
+
+    Add-Type -AssemblyName System.Drawing
+
+    Get-ChildItem -Path $SourceDirectory -Recurse -Include @("*.JPG", "*.AVI") | ForEach-Object {
+        Write-Verbose "Processing $($_.FullName)"
+
+        $file = $_
+        $dateTaken = $null
+
+        if ($file.Extension -eq ".JPG") {
+            try {
+                $img = [System.Drawing.Image]::FromFile($file.FullName)
+                # 36867 is the EXIF tag for DateTimeOriginal
+                $dateTakenStr = [System.Text.Encoding]::ASCII.GetString($img.GetPropertyItem(36867).Value).Trim([char]0)
+                $dateTaken = [datetime]::ParseExact($dateTakenStr, "yyyy:MM:dd HH:mm:ss", $null)
+                $img.Dispose()
+            } catch {
+                Write-Warning "Could not read EXIF data for $($file.Name), using LastWriteTime. Error: $_"
+                $dateTaken = $file.LastWriteTime
+            }
+            if (-not $dateTaken) {
+                Write-Warning "Could not determine date taken for $($file.Name), using LastWriteTime."
+                $dateTaken = $file.LastWriteTime
+            }
+        } else {
+            $dateTaken = $file.LastWriteTime
+        }
+
+        $formattedDate = $dateTaken.ToString("yyyyMMdd_HHmmss")
+        $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.fullname)
+        $fileExtension = [System.IO.Path]::GetExtension($file.fullname)
+
+        $newFileName = $fileNameWithoutExtension + "_" + $formattedDate + $fileExtension
+        $targetFolder = Join-Path -Path $DestinationDirectory -ChildPath ($dateTaken.ToString("yyyy-MM"))
+        
+        if (-not (Test-Path -Path $targetFolder)) {
+            if ($PSCmdlet.ShouldProcess($targetFolder, "Create new directory")) {
+                New-Item -ItemType Directory -Path $targetFolder | Out-Null
+            }
+        }
+
+        $targetPath = Join-Path -Path $targetFolder -ChildPath $newFileName
+        if ($PSCmdlet.ShouldProcess($file.FullName, "Move file to $targetPath")) {
+            Move-Item -Path $file.FullName -Destination $targetPath
+        }
+    }
+}
